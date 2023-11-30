c491d939e5db090aa9b71a49695d0a1ea2f0dc63
seocoal <jiyoon369@sookmyung.ac.kr>
2023-11-30 15:34:55 +0900

    1

--- main.c
+++ main.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct point{
+struct student{
 	
-	int x;
-	int y;
-	
+	int ID;
+	char name[100];
+	float grade;
 		
 };
 
 
 int main(int argc, char *argv[]) {
 	
-	struct point p1,p2;
-	int xdiff, ydiff;
-	double dist;
+	struct student s1 = {2211408, "JIYOON", 4.3};
 	
-	printf("input p1 cordinate (x y): ");
-	scanf("%d %d", &p1.x, &p1.y);
+	s1.ID= 123456;
+	s1.name[0]="c";
+	s1.grade=0.7;
 	
-	printf("input p1 cordinate (x y): ");
-	scanf("%d %d", &p2.x, &p2.y);
-	
-	xdiff=p2.x-p1.x;
-	ydiff=p2.y-p1.y;
-	
-	dist=sprt(xdiff*xdiff+ydiff*ydiff);
-	
-	printf("distance: %f\n", dist);
+	printf("ID: %i\n", s1.ID);
+	printf("name: %c\n", s1.name);
+	printf("grade: %f\nn", s1.grade);
 	
 
 	return 0;
